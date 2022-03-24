31a1d66d8ed47a950e3e918b6475f7a59ab0e025
NethmiDivyaT <nethmitharushika1999@gmail.com>
2022-03-24 12:12:50 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-function App() {
+import "react-toastify/dist/ReactToastify.css";
+import "../src/Component/style.css"
+toast.configure();
+
+export default function App() {
+  async function handleToken(token, addresses) {
+    const response = await axios.post(
+      { token}
+    );
+	console.log("Payment Done!!")
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <StripeCheckout
+        stripeKey="pk_test_51KfqVnB32MK4W6iDNeUGfW19fJfk9CJlJiDQomtCNsiCK8KDVybPyyLqhKqHBUO70jrG1pzQFXaWdMLhV6PKGXHE00umfgiErO"
+        token={handleToken}
+      />
     </div>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
